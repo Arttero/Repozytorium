11c6a8a89b3397225cd1a10427c1444090407d57
Arttero <98258413+Arttero@users.noreply.github.com>
2025-01-19 13:33:02 +0100

    Delete Lab8 directory

--- Lab8/Source.c
+++
@@ -1,7 +0,0 @@
-#include "Zadania.h"
-#include <stdio.h>
-
-int main() {
-	runMain();
-	return 0;
-}
--- Lab8/Zadania.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void runMain();
-void runZadanie1();
-void runZadanie2();
-void runZadanie3();
-void runZadanie4();
